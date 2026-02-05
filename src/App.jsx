4fff70028de317dc551b8c01ef058c83a04e738e
Larissa Ferreira <larissadferr@gmail.com>
2026-02-05 15:58:50 -0600

    updated imports - react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./components/welcome/Welcome";
 import { Login } from "./components/auth/Login";
+import { Authorized } from "./views/Authorized";
+import { ApplicationViews } from "./views/ApplicationViews";
 
 export const App = () => {
   return (
