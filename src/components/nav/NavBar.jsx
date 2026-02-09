925d30a7559b768326320717b7eb5aed9fd191ab
Larissa Ferreira <larissadferr@gmail.com>
2026-02-09 15:33:15 -0600

    NavBar - created

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,3 +1,29 @@
+import { Link, useNavigate } from "react-router-dom";
+
 export const NavBar = () => {
-  return <></>
-}
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        {localStorage.getItem("picup_user") ? (
+        <li>
+          <Link
+            to=""
+            onClick={() => {
+              localStorage.removeItem("picup_user")
+              navigate("/", { replace: true })
+            }}
+          >
+            Logout
+          </Link>
+        </li>
+      ) : (
+        ""
+      )}
+      </ul>
+    </>
+  );
+};
