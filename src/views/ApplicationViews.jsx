4439ad6e599723a88c60bca229c0f777af5e10e7
Larissa Ferreira <larissadferr@gmail.com>
2026-02-09 15:24:47 -0600

    ApplicationViews - set up routes

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+import { Welcome } from "../components/welcome/Welcome";
+import { NavBar } from "../components/nav/NavBar";
+
 export const ApplicationViews = () => {
-  return <></>
-}
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const localPicUpUser = localStorage.getItem("picup_user");
+    const picUpUserObject = JSON.parse(localPicUpUser);
+    setCurrentUser(picUpUserObject);
+  }, []);
+
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <NavBar />
+              <Outlet />
+            </>
+          }
+        >
+          <Route index element={<Welcome/>}/>
+        </Route>
+      </Routes>
+    </>
+  );
+};
