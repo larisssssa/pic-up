238b837d695066572d00e3255b5e44942706368d
Larissa Ferreira <larissadferr@gmail.com>
2026-02-12 15:03:22 -0600

    Project - created frame for project details

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+
 export const Project = () => {
-  return <></>
-}
+  return (
+    <>
+      <section>
+        <div>Project #</div>
+        <div>Client:</div>
+        <div>Status:</div>
+        <div>Session Date:</div>
+      </section>
+      <section>
+        <div>image</div>
+      </section>
+      <section>
+        <button>Save Changes</button>
+        <button>Delete Project</button>
+      </section>
+    </>
+  );
+};
