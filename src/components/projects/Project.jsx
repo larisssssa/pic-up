4a39f6bb33bacf573afcdf450c6a82c784107088
Larissa Ferreira <larissadferr@gmail.com>
2026-02-13 13:35:47 -0600

    Project - added project data refs

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getProjectById } from "../../services/projectService";
 
 export const Project = () => {
   const { projectId } = useParams();
+
+  const [projectArray, setProjectArray] = useState([]);
+  const [project, setProject] = useState({});
+
+  useEffect(() => {
+    getProjectById(projectId).then((p) => {
+      setProjectArray(p);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (projectArray.length == 1) {
+      setProject(projectArray[0]);
+    }
+  }, [projectArray]);
 
   return (
     <>
       <section>
         <div>Project # {projectId}</div>
-        <div>Client:</div>
-        <div>Status:</div>
-        <div>Session Date:</div>
+        <div>Client: {project.user?.name}</div>
+        <div>Status: {project.phase?.name}</div>
+        <div>Session Date: {project.date}</div>
       </section>
       <section>
         <div>image</div>
