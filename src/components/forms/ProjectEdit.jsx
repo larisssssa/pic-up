d3c727925182bbfc7c4b73ebf4445fc3731178c6
Larissa Ferreira <larissadferr@gmail.com>
2026-02-16 11:24:55 -0600

    ProjectEdit - created framework

--- src/components/forms/ProjectEdit.jsx
+++ src/components/forms/ProjectEdit.jsx
@@ -1,16 +1,59 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProjectById, getPhases } from "../../services/projectService";
+
 export const ProjectEdit = () => {
-  return <><select
-              value={updated.phase?.id}
-              onChange={(e) => {
-                const copy = { ...updated };
-                copy.phaseId = parseInt(e.target.value);
-                setUpdated(copy);
-              }}
-            >
-              {statusList.map((p) => (
-                <option key={p.id} value={p.id}>
-                  {p.name}
-                </option>
-              ))}
-            </select></>
-}
+  const { id } = useParams();
+  const [userChoices, setUserChoices] = useState({
+    id: 0,
+    name: "",
+    date: "",
+    userId: 0,
+    phaseId: 0,
+  });
+  const [phases, setPhases] = useState([]);
+
+  useEffect(() => {
+    getProjectById(id).then((p) => {
+      setUserChoices({
+        id: p.id,
+        name: p.name,
+        date: p.date,
+        userId: p.userId,
+        phaseId: p.phaseId,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    getPhases().then((phases) => {
+      setPhases(phases);
+    });
+  }, []);
+
+  return (
+    <>
+      <form>
+        <h2>Update Project</h2> <fieldset>Client Name:</fieldset>
+        <fieldset>
+          Status:
+          <select
+            value={userChoices.phase?.id}
+            onChange={(e) => {
+              const copy = { ...userChoices };
+              copy.phaseId = parseInt(e.target.value);
+              copy;
+            }}
+          >
+            {phases.map((p) => (
+              <option key={p.id} value={p.id}>
+                {p.name}
+              </option>
+            ))}
+          </select>
+        </fieldset>
+        <fieldset>Date:</fieldset>
+      </form>
+    </>
+  );
+};
