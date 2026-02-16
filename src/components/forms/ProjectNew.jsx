a88e56474a753b5e5f1ab48d9672f3e11dcd197d
Larissa Ferreira <larissadferr@gmail.com>
2026-02-16 15:57:41 -0600

    ProjectNew - created input fields, getUsers from userService for select dropdown

--- src/components/forms/ProjectNew.jsx
+++ src/components/forms/ProjectNew.jsx
@@ -1,17 +1,49 @@
+import { useEffect, useState } from "react";
+import { getUsers } from "../../services/userService";
+
 export const ProjectNew = () => {
+  const [users, setUsers] = useState([]);
+  const [userChoices, setUserChoices] = useState({
+    id: 0,
+    name: "",
+    date: "",
+    userId: 0,
+    phaseId: 0,
+  });
+
+  useEffect(() => {
+    getUsers().then((users) => {
+      setUsers(users);
+    });
+  }, []);
 
   return (
     <>
       <form>
         <h2>New Project</h2>
-        <fieldset>Project Name: </fieldset>
-        <fieldset>Client Id: </fieldset>
-        <fieldset>Client Name: </fieldset>
-        <fieldset>Session Date: </fieldset>
-
-        <fieldset>Date: </fieldset>
+        <fieldset>
+          <div>Project Name: </div>
+          <input type="text" id="name" />
+        </fieldset>
+        <fieldset>
+          <div>Client Name:</div>
+          <select>
+            <option key="0" value="0">
+              - Select Client -
+            </option>
+            {users.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+        </fieldset>
+        <fieldset>
+          <div>Session Date:</div>
+          <input type="date" />
+        </fieldset>
         <button>Save</button>
       </form>
     </>
   );
-}
+};
