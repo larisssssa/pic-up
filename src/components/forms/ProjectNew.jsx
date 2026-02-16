b9990cd33c341c91b8edf9f8b370224c2265e2dd
Larissa Ferreira <larissadferr@gmail.com>
2026-02-16 15:14:15 -0600

    ProjectNew - frame

--- src/components/forms/ProjectNew.jsx
+++ src/components/forms/ProjectNew.jsx
@@ -1,3 +1,17 @@
 export const ProjectNew = () => {
-  return <></>
+
+  return (
+    <>
+      <form>
+        <h2>New Project</h2>
+        <fieldset>Project Name: </fieldset>
+        <fieldset>Client Id: </fieldset>
+        <fieldset>Client Name: </fieldset>
+        <fieldset>Session Date: </fieldset>
+
+        <fieldset>Date: </fieldset>
+        <button>Save</button>
+      </form>
+    </>
+  );
 }
