7e736fc56bc9bde1115c5628cc4fbd5889a03148
Larissa Ferreira <larissadferr@gmail.com>
2026-02-16 14:23:27 -0600

    ProjectEdit - added cap to handle changes; projectService - saveProjectUpdates put call

--- src/components/forms/ProjectEdit.jsx
+++ src/components/forms/ProjectEdit.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getProjectById, getPhases } from "../../services/projectService";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  getProjectById,
+  getPhases,
+  saveProjectUpdates,
+} from "../../services/projectService";
 
 export const ProjectEdit = () => {
+  const navigate = useNavigate();
   const { projectId } = useParams();
+
   const [project, setProject] = useState({});
-  const [userChoices, setUserChoices] = useState({});
+  const [userChoices, setUserChoices] = useState({
+    id: 0,
+    name: "",
+    date: "",
+    userId: 0,
+    phaseId: 0,
+  });
   const [phases, setPhases] = useState([]);
 
   useEffect(() => {
@@ -28,6 +40,15 @@ export const ProjectEdit = () => {
     });
   }, []);
 
+  const handleSave = (e) => {
+    e.preventDefault();
+    if (userChoices.phaseId != 0) {
+      saveProjectUpdates(userChoices).then(navigate(-1));
+    } else {
+      alert("Please select status");
+    }
+  };
+
   return (
     <>
       <form>
@@ -43,6 +64,9 @@ export const ProjectEdit = () => {
               setUserChoices(copy);
             }}
           >
+            <option key="0" value="0">
+              - select status -
+            </option>
             {phases.map((p) => (
               <option key={p.id} value={p.id}>
                 {p.name}
@@ -51,7 +75,7 @@ export const ProjectEdit = () => {
           </select>
         </fieldset>
         <fieldset>Date: {project.date}</fieldset>
-        <button>Save</button>
+        <button onClick={(e) => handleSave(e)}>Save</button>
       </form>
     </>
   );
