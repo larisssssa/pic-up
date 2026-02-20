bf278938923ec345330f9f1372659ba568439922
Larissa Ferreira <larissadferr@gmail.com>
2026-02-20 15:14:23 -0600

    css - welcome component

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,20 +1,19 @@
 import { Link } from "react-router-dom";
+import "./Welcome.css";
 
 export const Welcome = () => {
   return (
     <>
-      <main>
-        <header>
-          <div>
-            <h3>Welcome to</h3>
-          </div>
-          <div>
-            <h1>PicUp</h1>
-          </div>
-        </header>
-        <h4>A Business Management Tool for Photographers</h4>
-        <Link to={`/login`}>Login</Link>
-      </main>
+      <div className="welcome-main">
+        <div className="welcome-header">
+          <h3>Welcome to</h3>
+          <h1>PicUp</h1>
+          <p>A Business Management Tool for Photographers</p>
+        </div>
+        <div className="welcome-login">
+          <Link to={`/login`}>Login</Link>
+        </div>
+      </div>
     </>
   );
 };
