c8c5ad365262ac4a60ee9bd96e5e2b3d9ae94642
Larissa Ferreira <larissadferr@gmail.com>
2026-03-02 19:20:09 -0600

    NavBar css - updated class names, implemented flex box

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,19 +1,20 @@
 import { Link, useNavigate } from "react-router-dom";
+import "./NavBar.css";
 
 export const NavBar = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <ul>
-        <li>
+    <div className="nav">
+      <ul className="nav-bar">
+        <li className="nav-home">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="nav-item">
           <Link to="/projects">Projects</Link>
         </li>
         {localStorage.getItem("picup_user") ? (
-          <li>
+          <li className="nav-logout">
             <Link
               to=""
               onClick={() => {
@@ -28,6 +29,6 @@ export const NavBar = () => {
           ""
         )}
       </ul>
-    </>
+    </div>
   );
 };
